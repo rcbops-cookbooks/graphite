ba0ed2d633833586726a9e73f8bfe3a4c4276171
rcbjenkins <rcb-deploy@lists.rackspace.com>
2012-07-12 17:06:41 -0500

    End of sprint version bump.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "osops@lists.launchpad.net"
 license          "Apache 2.0"
 description      "Installs/Configures graphite"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
 
 %W{apt apache2 osops-utils yum}.each do |dep|
   depends dep
